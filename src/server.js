11bae01d1d329e2792d0ab5bdd824bb9b502f9a1
Gustavo Miguelote <gustavomiguelote@MacBook-Air-de-Gustavo-Miguelote.local>
2019-10-03 21:19:02 -0300

    initial commit ominiStack week backend

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   return res.send({message:"hello word"})
+});
+
+app.listen(3333, () => {
+   console.log("Backend online");
+});
