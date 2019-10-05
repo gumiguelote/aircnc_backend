7f799391461c6ed3b21e1be066f289d8755fad55
Gustavo Miguelote <gustavomiguelote@MacBook-Air-de-Gustavo-Miguelote.local>
2019-10-04 23:53:57 -0300

    Backend Aircnc

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-   return res.send({message:"hello word"})
-});
+mongoose
+.connect('mongodb+srv://usrmongo:usrmongo@clustermiguelote-gb3nk.mongodb.net/omnistack?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
    console.log("Backend online...");
