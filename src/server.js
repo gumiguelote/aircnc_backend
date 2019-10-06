8f6860808b89fe6bdcf514039778399354bc4a89
Gustavo Miguelote <gustavomiguelote@MacBook-Air-de-Gustavo-Miguelote.local>
2019-10-06 11:19:43 -0300

    added express static for uploads

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const path = require('path');
 
 const USERMONGO = process.env.USERMONGO;
 const PASSMONGO = process.env.PASSMONGO;
@@ -11,12 +12,12 @@ const DBNAME = process.env.DBNAME;
 
 const app = express();
 
-mongoose
-.connect(`mongodb+srv://${USERMONGO}:${PASSMONGO}@${DBNAME}?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${USERMONGO}:${PASSMONGO}@${DBNAME}?retryWrites=true&w=majority`,
 { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => {
