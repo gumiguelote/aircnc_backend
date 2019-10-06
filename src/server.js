5d8620821687e279776cb05a3dfd100995ebb5ac
Gustavo Miguelote <gustavomiguelote@MacBook-Air-de-Gustavo-Miguelote.local>
2019-10-06 00:28:51 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose
 .connect('mongodb+srv://usrmongo:usrmongo@clustermiguelote-gb3nk.mongodb.net/omnistack?retryWrites=true&w=majority',
 { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
 
